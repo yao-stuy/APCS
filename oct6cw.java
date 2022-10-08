8117239c0e3752058b8f166c8acdedd176cb55a0
Yao Chen <ychen41@stuy.edu>
2022-10-08 19:46:23 -0400

    n

--- oct6cw.java
+++ oct6cw.java
@@ -8,7 +8,26 @@ public class oct6cw{
   }
 
   public static int[] concatArray(int[]ary1, int[]ary2){
-      return ary1;
+    int[] nArray;
+    if (ary1.length>ary2.length){
+      nArray = new int[ary1.length];
+      for (int x = 0; x < ary2.length; x++){
+        nArray[x] = ary1[x] + ary2[x];
+      }
+      for (int x = ary2.length; x < ary1.length; x++){
+        nArray[x] = ary1[x];
+      }
+    }
+    else{
+      nArray = new int[ary2.length];
+      for (int x = 0; x < ary1.length; x++){
+        nArray[x] = ary1[x] + ary2[x];
+      }
+    for (int x = ary1.length; x < ary2.length; x++){
+      nArray[x] = ary2[x];
+    }
+  }
+    return nArray;
   }
 
 

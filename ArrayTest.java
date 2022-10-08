f7a9f060276b9eb11f16350f2506e9295c2ab9d4
Yao Chen <ychen41@stuy.edu>
2022-10-08 19:26:07 -0400

    q

--- ArrayTest.java
+++ ArrayTest.java
@@ -9,6 +9,15 @@ public class ArrayTest{
      System.out.println("part 1: " + anArray);
      //print the array variable\
      System.out.print("part 2: ");
+     for (int x = 0; x < anArray.length; x+=5){
+       if (x==10){
+          System.out.println(anArray[x] + " ");
+       }
+      else{
+     System.out.print(anArray[x] + " ");
+   }
+   }
+     System.out.println("part 3: ");
      for (int x = 0; x < anArray.length; x++){
      System.out.print(anArray[x] + " ");
    }
